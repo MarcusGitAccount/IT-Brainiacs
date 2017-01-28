1a4266eb5c93fddccee2a2a35d02e3b058330d0e
MarcusGtAccount <pop_marcus_98@yahoo.com>
2017-01-28 23:55:27 +0200

    working on employees table

--- atestat/public/js/employees.js
+++ atestat/public/js/employees.js
@@ -3,6 +3,7 @@ const adminTable = document.querySelector('table.administrators');
 const employeesTables = document.querySelector('table.employees');
 const arrowsUp = document.querySelectorAll('i.fa-arrow-up');
 const arrowsDown = document.querySelectorAll('button>i.fa-arrow-down');
+const insideInputs = document.querySelectorAll('td');
 const getTables = {
   administrators: getAdmins,
   employees: getEmployees
@@ -11,13 +12,13 @@ const getTables = {
 function getAdmins(sort = 'ASC', col = 'id') {
   $.get(`/api/administrators?${col}=${sort}`, data => {
     populateAdminTable(data);
-    console.log(data);
   });
 }
 
 function getEmployees(sort = 'ASC', col = 'id') {
   $.get(`/api/employees?${col}=${sort}`, data => {
-
+    populateEmployeesTable(data);
+    console.log(data);
   });
 }
 
@@ -29,8 +30,14 @@ function populateAdminTable(data) {
   });
 }
 
-function populateTables() {
-  // data-key data-column
+function populateEmployeesTable(data) {
+  // key is the first item in the row (id)
+
+  while (employeesTables.childElementCount > 1)
+    employeesTables.removeChild(employeesTables.lastChild);
+  data.rows.forEach((row, index) => {
+    employeesTables.innerHTML += `<tr><td>${row.id}</td><td>${row.first_name}</td><td>${row.last_name}</td><td><input data-col="phone_number" data-original="${row.phone_number}" type="text" value="${row.phone_number}" pattern="[\d\s]+"></td><td><input data-col="salary" data-original="${row.salary}" type="text" value="${row.salary}" pattern="[\d\.]+"></td><td><input data-col="job_name" data-original="${row.job_name}" type="text" value="${row.job_name}" pattern="[\w]+"></td><td><input data-col="email" data-original="${row.email}" type="text" value="${row.email}" pattern="[\w]+@[\w]+"></td></tr>`;
+  });
 }
 
 function arrowSortAsc(i) {
@@ -41,7 +48,9 @@ function arrowSortDesc(i) {
   getTables[i.dataset.table]('DESC', i.dataset.col);
 }
 
+
 //arrowsUp.forEach(arrow => {arrow.addEventListener('click', arrowSortAsc); console.log(arrow)});/
 //arrowsDown.forEach(arrow => arrow.addEventListener('click', arrowSortDesc));
 
 getAdmins();
+getEmployees();
