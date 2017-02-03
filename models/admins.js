cde470e0d827acd627c6aeeeaa3c77394c34fed8
Marcus Pop <pop_marcus_98@yahoo.com>
2017-02-03 10:10:31 +0000

    makePromise

--- models/admins.js
+++ models/admins.js
@@ -1,6 +1,7 @@
 
 const mysql = require("mysql");
 const tableName = 'administrators';
+const columns = ['id', 'name', 'password', 'email', 'registration_date'];
 const connection = mysql.createConnection({
   'host': '0.0.0.0',
   'user': 'marcuspop',
@@ -11,80 +12,45 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+function makePromise(statement) {
+  return new Promise((resolve, reject) => {
+    connection.query(statement, (error, result, fields) => {
+      if (error) {
+        reject(error);
+        return ;
+      }
+      
+      resolve(result, fields);
+    });
+  });
+}
+
 module.exports = {
+  'tableName': tableName,
+  'columns': columns,
   'selectAll': () => {
     const statement = 'select * from administrators';
-    return new Promise((resolve, reject) => {
-      connection.query(statement, (error, result, fields) => {
-        if (error) {
-          reject(error);
-          return ;
-        }
-        
-        resolve(result, fields);
-      });
-    });
+    
+    return makePromise(statement);
   },
   'selectByColumns': (where) => {/* ?? = ? */
-
     const statement = mysql.format(`select * from administrators where ?`, where);
 
-    return new Promise((resolve, reject) => {
-      connection.query(statement, (error, result, fields) => {
-        if (error) {
-          reject(error);
-          return ;
-        }
-        
-        resolve(result, fields);
-      });
-    });
+    return makePromise(statement);
   },
   'insert': (data) => {
     const statement = mysql.format(`insert into administrators set ?`, data);
     
-    console.log(statement);
-    return new Promise((resolve, reject) => {
-      connection.query(statement, (error, result, fields) => {
-        if (error) {
-          reject(error);
-          console.log(error)
-          return ;
-        }
-        
-        resolve(result, fields);
-      });
-    });
+    return makePromise(statement);
   },
   'update': (what, where) => {
     const statement = mysql.format('update administrators set ? where ?', [what, where]);
     
-    console.log(statement);
-    return new Promise((resolve, reject) => {
-      connection.query(statement, (error, result, fields) => {
-        if (error) {
-          reject(error);
-          console.log(error)
-          return ;
-        }
-        
-        resolve(result, fields);
-      });
-    });
+    return makePromise(statement);
   },
   'delete': (where) => {
     const statement = mysql.format('delete from administrators where ?', where);
     
-    console.log(statement);
-    return new Promise((resolve, reject) => {
-      connection.query(statement, (error, result, fields) => {
-        if (error) {
-          reject(error);
-          return ;
-        }
-        
-        resolve(result, fields);
-      });
-    });
+    return makePromise(statement);
   }
-}
+};
